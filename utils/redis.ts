fdff652438f9ff5a94b5a3d8f5635aac05321cc7
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-09-26 23:55:27 +0600

    add:Error handler

---
+++ utils/redis.ts
@@ -0,0 +1,11 @@
+
+import { Redis } from "ioredis";
+require("dotenv").config();
+const redisClient = () =>{
+     if(process.env.REDIS_URI){
+        console.log(`Redis connected`);
+        return process.env.REDIS_URI;
+     }
+     throw new Error('Redis connection faild');
+};
+export const redis =  new Redis(redisClient());
