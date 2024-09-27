6f9878a21243aaf5393199125949eeaeb1321963
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-09-28 01:00:16 +0600

    add:catch AsyncError eliment

---
+++ middleware/catchAsyncErrors.ts
@@ -0,0 +1,5 @@
+import { NextFunction } from "express";
+
+export const catchAsyncError = (theFunc:any)=>{( req:Request,res:Response,next:NextFunction) =>
+    Promise.resolve(theFunc(req,res,next)).catch(next);
+}
