99649bd0fdb4d357f625e007b7f46d70617e93da
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-10-07 01:35:32 +0600

    add:user update

--- controllers/user.controllers.ts
+++ controllers/user.controllers.ts
@@ -278,6 +278,46 @@ export const socialAuth =
   }
 ;
 
+// update user info
+
+interface IUpdateUserInfo {
+  name?: string;
+  email?: string;
+}
+
+export const updateUserInfo = 
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email } = req.body as IUpdateUserInfo;
+      const userId : any = req.user?._id;
+      const user = await UserModel.findById(userId);
+
+      if (email && user) {
+        const isEmailExist = await UserModel.findOne({ email });
+        if (isEmailExist) {
+          return next(new ErrorHandler("Email already exists", 400));
+        }
+        user.email = email;
+      }
+
+      if (name && user) {
+        user.name = name;
+      }
+
+      await user?.save();
+      await redis.set(userId, JSON.stringify(user));
+
+      res.status(201).json({
+        success: true,
+        user,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+;
+
+
 
 
 
--- services/user.services.ts
+++ services/user.services.ts
@@ -1,20 +1,15 @@
 import { Response } from "express";
-import UserModel from "../models/user.model";
+import userModel from "../models/user.model"; // Fixed typo "user Model" -> "userModel"
+import { redis } from "../utils/redis";
 
-
-// Get user by ID
-export const getUserById = async (id: string, res: Response) => {
-  try {
-    const user = await UserModel.findById(id);
-
-    res.status(201).json({
-      success: true,
-      user,
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
+// get user by id
+export const getUserById = async (id: string, res: Response): Promise<void> => {
+    const userJson = await redis.get(id); // Fixed "user son" -> "userJson"
+    if (userJson) { // Fixed "if (user)son)" -> "if (userJson)"
+        const user = JSON.parse(userJson); // Fixed "user]son" -> "userJson"
+        res.status(201).json({
+            success: true,
+            user,
+        });
+    }
 };
