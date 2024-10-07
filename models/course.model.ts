7b351c0354aea54a7a2475f8197771e0ff648f48
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-10-07 23:13:45 +0600

    add:course model

---
+++ models/course.model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface IComment extends Document {
+  user: object;
+  comment: string;
+}
+
+interface IReview extends Document {
+  user: object;
+  rating: number;
+  comment: string;
+  commentReplies: IComment[];
+}
+
+interface ILink extends Document {
+  title: string;
+  url: string;
+}
+
+interface ICourseData extends Document {
+  title: string;
+  description: string;
+  videoUrl: string;
+  videoThumbnail: object;
+  videoSection: string;
+  videoLength: number;
+  videoPlayer: string;
+  links: ILink[];
+  suggestion: string;
+  questions: IComment[];
+}
