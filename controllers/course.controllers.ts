5ae5083092cc3b5173866ab5eb9524d5b2e6695c
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-10-09 03:38:39 +0600

    add:add:cours conttrols

---
+++ controllers/course.controllers.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
+import cloudinary from "cloudinary";
+import { createCourse } from "../services/course.service";
+
+// Upload course
+export const uploadCourse = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const data = req.body;
+    const thumbnail = data.thumbnail;
+
+    if (thumbnail) {
+      const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+        folder: "courses"
+      });
+
+      data.thumbnail = {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      };
+    }
+
+    await createCourse(data, res, next);
+  } catch (error: any) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
