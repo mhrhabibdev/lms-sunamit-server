94146028f64adbbcf0982d6ea6d00fadd5029f2c
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-10-19 05:40:58 +0600

    add:updateNotification

--- controllers/notification.controllers.ts
+++ controllers/notification.controllers.ts
@@ -14,3 +14,27 @@ export const getNotifications = async (req: Request, res: Response, next: NextFu
     return next(new ErrorHandler(error.message, 500));
   }
 };
+
+// update notification status --only admin
+export const updateNotification =
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const notification = await NotificationModel.findById(req.params.id);
+        if (!notification) {
+          return next(new ErrorHandler("Notification not found", 404));
+        } else {
+          notification.status ? (notification.status = "read") : notification?.status;
+        }
+        await notification.save();
+        const notifications = await NotificationModel.find().sort({
+          createdAt: -1,
+        });
+        res.status(201).json({
+          success: true,
+          notifications,
+        });
+      } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    };
+  
--- routes/notification.route.ts
+++ routes/notification.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../models/auth";
-import { getNotifications } from "../controllers/notification.controllers";
+import { getNotifications, updateNotification } from "../controllers/notification.controllers";
 
 
 const notificationRoute = express.Router();
 
 notificationRoute.get("/get-all-notifications", isAuthenticated, authorizeRoles("admin"), getNotifications);
+notificationRoute.put("/update-notifications/:id", isAuthenticated, authorizeRoles("admin"), updateNotification);
 
 export default notificationRoute;
